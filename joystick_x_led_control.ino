04cc170e2efa1902fb969b0c54e42514349af4f5
Abdurrahman <abdurrahmanopr@gmail.com>
2026-08-22 14:58:55 +0300

    Add push-button control and extra LED pin
    Updated joystick LED control to include a push-button and an additional LED pin.

--- joystick_x_led_control.ino
+++ joystick_x_led_control.ino
@@ -1,25 +1,42 @@
-// Arduino Uno & XY Joystick ile X Ekseni LED Kontrolü
-const int led_pin[] = { 8, 9, 10 };
+// [TR] Arduino Uno & XY Joystick ile X Ekseni ve Buton Kontrollü LED Sistemi
+// [EN] Arduino Uno & XY Joystick X-Axis and Push-Button LED Control System
+
+const int led_pin[] = { 8, 9, 10, 13 };
+const int butonPin = 2;
+
 void setup() {
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 4; i++) {
     pinMode(led_pin[i], OUTPUT);
   }
+  pinMode(butonPin, INPUT_PULLUP);
 }
-// Arduino'nun analogRead(A0) fonksiyonu bize 0 ile 1023 arasında bir değer okur.
-void loop() {
-  int xdeger = analogRead(A0);
-  if (xdeger < 300) {
-    digitalWrite(led_pin[0], HIGH);
-    digitalWrite(led_pin[1], LOW);
-    digitalWrite(led_pin[2], LOW);
-  } else if (xdeger > 700) {
-    digitalWrite(led_pin[2], HIGH);
-    digitalWrite(led_pin[0], LOW);
-    digitalWrite(led_pin[1], LOW);
 
-  } else {
-    digitalWrite(led_pin[1], HIGH);
+void loop() {
+  if (digitalRead(butonPin) == LOW) {
+    digitalWrite(led_pin[3], HIGH);
+    digitalWrite(led_pin[1], LOW);
     digitalWrite(led_pin[2], LOW);
     digitalWrite(led_pin[0], LOW);
+  } else {
+    digitalWrite(led_pin[3], LOW);
+
+
+
+
+    int xdeger = analogRead(A0);
+
+    if (xdeger < 300) {
+      digitalWrite(led_pin[0], HIGH);
+      digitalWrite(led_pin[1], LOW);
+      digitalWrite(led_pin[2], LOW);
+    } else if (xdeger > 700) {
+      digitalWrite(led_pin[2], HIGH);
+      digitalWrite(led_pin[0], LOW);
+      digitalWrite(led_pin[1], LOW);
+    } else {
+      digitalWrite(led_pin[1], HIGH);
+      digitalWrite(led_pin[2], LOW);
+      digitalWrite(led_pin[0], LOW);
+    }
   }
 }
